e2ab0fc916a40e0924aebf425809352aad6946b9
Daniel Henrique <ddanielhpf@gmail.com>
2022-05-04 13:15:57 -0300

    ✨ feat: criação do menu
    Funções com JavaScript e ajuste com CSS

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// Função de habilitar meenu.
 function onScroll() {
-    console.log("ALOOOOOO CARAI")
+  if (scrollY > 0) {
+    navigation.classList.add('scroll') //Add Scroll na class
+  } else {
+    navigation.classList.remove('scroll') //Remove Scroll da class
+  }
+}
+
+//Função abrir menu
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+//Função fecha menu menu
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
 }
